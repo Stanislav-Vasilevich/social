0a038e99533aa936e3e3d3aa9f229d44928291f8
Stanislav <stanislavasilevich@gmail.com>
2021-06-23 18:02:18 +0300

    feat: сделать общую разметку страницы

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,19 @@ import FaceSection from "./FaceSection";
 const App = () => {
   return (
     <div className="app">
-      <div className="page">
-        <Header/>
-        <FaceSection/>
+      <Header/>
+      <div className="body">
+        <FaceSection />
+        <div className="page">
+          <aside className="side-bar">
+            <div className="side-bar__brief-information">Краткая информация</div>
+            <div className="side-bar__photo">Фото</div>
+            <div className="side-bar__friends">Друзья</div>
+          </aside>
+          <main className="main">
+            главный контент
+          </main>
+        </div>
       </div>
     </div>
   );
