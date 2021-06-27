807ee58f9a9dc9e26070fc37ff6c724bc373e960
Stanislav <stanislavasilevich@gmail.com>
2021-06-27 22:58:53 +0300

    feat: добавить динамические тексты и картинки в посты

--- src/components/Main/Posts/Post/Post.tsx
+++ src/components/Main/Posts/Post/Post.tsx
@@ -3,11 +3,16 @@ import styles from "./Post.module.css";
 
 const {section, avatar, text} = styles;
 
-function Post() {
+type PostPropsType = {
+  title: string,
+  img: string
+}
+
+const Post: React.FC<PostPropsType> = (props) => {
   return (
     <div className={section}>
-      <img className={avatar} src={'https://s.starladder.com/uploads/user_logo/b/f/2/d/meta_tag_d6ca03e719804347cb71d8338d5bce5a.jpg'}></img>
-      <p className={text}>Текст</p>
+      <img className={avatar} src={props.img}></img>
+      <p className={text}>{props.title}</p>
     </div>
   )
 }
--- src/components/Main/Posts/Posts.tsx
+++ src/components/Main/Posts/Posts.tsx
@@ -7,10 +7,10 @@ const {section} = styles;
 function Posts() {
   return (
     <div className={section}>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post title={"Аффирмация на каждый день"} img={"https://im0-tub-ru.yandex.net/i?id=70bde5566a6078f6bca01933fc2a412f&ref=rim&n=33&w=300&h=300"} />
+      <Post title={"Программирование на javascript"} img={"https://sun9-10.userapi.com/xMtkKxtrmIts_wJ8cOH_rhasMaGGftbw6m3XEg/mFWBoDS8ZjI.jpg"} />
+      <Post title={"Обучение финансовой грамотности"} img={"https://yt3.ggpht.com/ytc/AAUvwnjbnydVEHlADfNrG_bVJP6GvmlelF9MwZczb10h2g=s900-c-k-c0x00ffffff-no-rj"} />
+      <Post title={"Поездка на Бали"} img={"https://yt3.ggpht.com/a/AATXAJzp71mv5MJ56d1-PrE7tL31GVRDbGP73QmS1Q=s900-c-k-c0xffffffff-no-rj-mo"} />
     </div>
   )
 }
