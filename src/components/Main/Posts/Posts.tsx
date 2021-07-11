ff659c7b5de1172e621bb279365ff2ce6a86b4af
Stanislav <stanislavasilevich@gmail.com>
2021-07-11 13:16:24 +0300

    feat: вывести объекты постов, друзей и диалогов в компонентах через метод map

--- src/components/Main/Posts/Posts.tsx
+++ src/components/Main/Posts/Posts.tsx
@@ -12,19 +12,38 @@ function Posts() {
     likesCount: number
   }
 
-  const postData: Array<postDataType> = [
-    {id: 1, title:"Аффирмация на каждый день и далее пошел целый сплошной текст, который приводит к каким-то посдедствиям и заставляет нас применить свои усилия, чтобы понять, что тут вобще написано", img:"https://im0-tub-ru.yandex.net/i?id=70bde5566a6078f6bca01933fc2a412f&ref=rim&n=33&w=300&h=300", likesCount: 4 },
-    {id: 2, title:"Программирование на javascript и далее пошел целый сплошной текст, который приводит к каким-то посдедствиям и заставляет нас применить свои усилия, чтобы понять, что тут вобще написано", img:"https://sun9-10.userapi.com/xMtkKxtrmIts_wJ8cOH_rhasMaGGftbw6m3XEg/mFWBoDS8ZjI.jpg", likesCount: 1},
-    {id: 3, title:"Обучение финансовой грамотности и далее пошел целый сплошной текст, который приводит к каким-то посдедствиям и заставляет нас применить свои усилия, чтобы понять, что тут вобще написано", img:"https://yt3.ggpht.com/ytc/AAUvwnjbnydVEHlADfNrG_bVJP6GvmlelF9MwZczb10h2g=s900-c-k-c0x00ffffff-no-rj", likesCount: 3},
-    {id: 4, title:"Поездка на Бали и далее пошел целый сплошной текст, который приводит к каким-то посдедствиям и заставляет нас применить свои усилия, чтобы понять, что тут вобще написано", img:"https://yt3.ggpht.com/a/AATXAJzp71mv5MJ56d1-PrE7tL31GVRDbGP73QmS1Q=s900-c-k-c0xffffffff-no-rj-mo", likesCount: 2}
+  const posts: Array<postDataType> = [
+    {
+      id: 1,
+      title: "Аффирмация на каждый день и далее пошел целый сплошной текст, который приводит к каким-то посдедствиям и заставляет нас применить свои усилия, чтобы понять, что тут вобще написано",
+      img: "https://im0-tub-ru.yandex.net/i?id=70bde5566a6078f6bca01933fc2a412f&ref=rim&n=33&w=300&h=300",
+      likesCount: 4
+    },
+    {
+      id: 2,
+      title: "Программирование на javascript и далее пошел целый сплошной текст, который приводит к каким-то посдедствиям и заставляет нас применить свои усилия, чтобы понять, что тут вобще написано",
+      img: "https://sun9-10.userapi.com/xMtkKxtrmIts_wJ8cOH_rhasMaGGftbw6m3XEg/mFWBoDS8ZjI.jpg",
+      likesCount: 1
+    },
+    {
+      id: 3,
+      title: "Обучение финансовой грамотности и далее пошел целый сплошной текст, который приводит к каким-то посдедствиям и заставляет нас применить свои усилия, чтобы понять, что тут вобще написано",
+      img: "https://yt3.ggpht.com/ytc/AAUvwnjbnydVEHlADfNrG_bVJP6GvmlelF9MwZczb10h2g=s900-c-k-c0x00ffffff-no-rj",
+      likesCount: 3
+    },
+    {
+      id: 4,
+      title: "Поездка на Бали и далее пошел целый сплошной текст, который приводит к каким-то посдедствиям и заставляет нас применить свои усилия, чтобы понять, что тут вобще написано",
+      img: "https://yt3.ggpht.com/a/AATXAJzp71mv5MJ56d1-PrE7tL31GVRDbGP73QmS1Q=s900-c-k-c0xffffffff-no-rj-mo",
+      likesCount: 2
+    }
   ]
+
+  const arrayPosts = posts.map(i => <Post id={i.id} title={i.title} img={i.img} likesCount={i.likesCount}/>)
 
   return (
     <div className={section}>
-      <Post id={postData[0].id} title={postData[0].title} img={postData[0].img} likesCount={postData[0].likesCount} />
-      <Post id={postData[1].id} title={postData[1].title} img={postData[1].img} likesCount={postData[1].likesCount} />
-      <Post id={postData[2].id} title={postData[2].title} img={postData[2].img} likesCount={postData[2].likesCount} />
-      <Post id={postData[3].id} title={postData[3].title} img={postData[3].img} likesCount={postData[3].likesCount} />
+      {arrayPosts}
     </div>
   )
 }
--- src/components/Page/Friends/Friends.tsx
+++ src/components/Page/Friends/Friends.tsx
@@ -3,7 +3,7 @@ import styles from "./Friends.module.css";
 import Friend from "./Friend/Friend";
 import Dialogs from "./Dialogs/Dialogs";
 
-const {main, page, people, dialogs} = styles;
+const {main, page, people, dialogsStyle} = styles;
 
 function Friends() {
   type friendDataType = {
@@ -12,17 +12,25 @@ function Friends() {
     img: string
   }
 
-  const friendsData: Array<friendDataType> = [
+  type dialogsDataType = {
+    id: number
+    message: string
+  }
+
+  const friends: Array<friendDataType> = [
     {id: 1, name: 'Катя', img: 'https://avatarko.ru/img/kartinka/16/Joker_minion_15661.jpg'},
     {id: 2, name: 'Паша', img: 'https://klike.net/uploads/posts/2018-06/1528369868_15.jpg'},
     {id: 3, name: 'Вова', img: 'https://pbs.twimg.com/media/Ce43PAzW4AA_g9g.jpg'}
   ]
 
-  const dialogsData = [
-    {id: 1, message: "Hello! How are you?"},
-    {id: 2, message: "Hey! I`m work, I`m find!"},
-    {id: 3, message: "It`s good))"}
+  const dialogs:Array<dialogsDataType> = [
+    {id: 1, message: 'Hello! How are you?'},
+    {id: 2, message: 'Hey! I`m work, I`m find!'},
+    {id: 3, message: 'It`s good))'}
   ]
+
+  const arrayFriends = friends.map(i => <Friend id={i.id} img={i.img} name={i.name} />)
+  const arrayDialogs = dialogs.map(i => <Dialogs id={i.id} message={i.message} />)
 
   return (
     <section className={main}>
@@ -31,43 +39,13 @@ function Friends() {
       {/* блок с друзьями */}
       <div className={page}>
         <div className={people}>
-
-          {/* Даша */}
-          <Friend
-            id={friendsData[0].id}
-            img={friendsData[0].img}
-            name={friendsData[0].name}
-          />
-
-          {/* Паша */}
-          <Friend
-            id={friendsData[1].id}
-            img={friendsData[1].img}
-            name={friendsData[1].name}
-          />
-
-          {/* Вова */}
-          <Friend
-            id={friendsData[2].id}
-            img={friendsData[2].img}
-            name={friendsData[2].name}
-          />
+          {/* друзья */}
+          {arrayFriends}
         </div>
 
         {/* Диалоги */}
-        <div className={dialogs}>
-          <Dialogs
-            id={dialogsData[0].id}
-            message={dialogsData[0].message}
-          />
-          <Dialogs
-            id={dialogsData[1].id}
-            message={dialogsData[1].message}
-          />
-          <Dialogs
-            id={dialogsData[2].id}
-            message={dialogsData[2].message}
-          />
+        <div className={dialogsStyle}>
+          {arrayDialogs}
         </div>
       </div>
     </section>
