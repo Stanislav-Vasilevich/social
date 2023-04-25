3d3224d5104134a7199d3cc8d080c0af40a9871e
StanislaVailevich <stanislavasilevich@yandex.ru>
2023-04-25 12:10:38 +0300

    добавил картинку в меню на главную страницу

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -2,29 +2,44 @@ import React from 'react';
 import s from './Menu.module.css';
 import {NavLink} from "react-router-dom";
 import {CommunityPageNavigationType} from '../../../redux/сommunity-reducer';
+import imageHome from './../../../image/home.webp';
 
 type PropsType = {
-  menuItems: Array<CommunityPageNavigationType>
+	menuItems: Array<CommunityPageNavigationType>
 }
 
 const Menu = (props: PropsType) => {
-  const menuItem = props.menuItems.map(i => {
-    return (
-      <NavLink
-        key={i.id}
-        className={(params) => params.isActive ? s.active : s.item}
-        to={i.path}
-      >
-        {i.item}
-      </NavLink>
-    )
-  });
+	const menuItem = props.menuItems.map(i => {
+		console.log('i: ', i.path);
 
-  return (
-    <nav className={s.menu}>
-      {menuItem}
-    </nav>
-  );
+		if (i.path === '/') {
+			return (
+				<NavLink
+					key={i.id}
+					className={s.itemHome}
+					to={i.path}
+				>
+					<img className={s.itemHome__img} src={imageHome} alt="home"/>
+				</NavLink>
+			)
+		}
+
+		return (
+			<NavLink
+				key={i.id}
+				className={(params) => params.isActive ? s.active : s.item}
+				to={i.path}
+			>
+				{i.item}
+			</NavLink>
+		)
+	});
+
+	return (
+		<nav className={s.menu}>
+			{menuItem}
+		</nav>
+	);
 }
 
 export default Menu;
