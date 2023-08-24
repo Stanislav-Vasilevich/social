3aa2d6aa53583b0bc4c3959a8b7abec444d1429d
Stanislav <stanislavasilevich@gmail.com>
2023-08-24 10:53:25 +0300

    save

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Button } from './Button';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
 
 export default {
   title: 'Example/Button',
